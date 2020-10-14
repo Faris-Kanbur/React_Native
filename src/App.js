e71b19f07a17fec6810a91a8840d3a1e609161e3
faris <faris38kanbur@gmail.com>
2020-10-14 21:14:27 +0200

    feat: added new using state code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('faris');
-  
+  const [person, setPerson] = useState({name: 'mario', age: 40});
 
   const clicHander = () => {
     setName('kanbur');
+    setPerson({name: 'selim', age: 18});
   };
 
   return (
     <View style={styles.container}>
       <Text>My name is {name}</Text>
+      <Text>
+        My name is {person.name} and my age is {person.age}
+      </Text>
       <View style={styles.buttonContainer}>
         <Button title="update state" onPress={clicHander} />
       </View>
