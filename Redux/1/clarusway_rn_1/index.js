a1428a4326688f02a3f8f6b91c868fde7dd57ca6
faris <faris38kanbur@gmail.com>
2020-11-24 19:26:23 +0100

    delete

--- Redux/1/clarusway_rn_1/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
-import Main from './src/Main';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Main);
--- Redux/1/clarusway_rn_1/src/context/store.js
+++
@@ -1,5 +0,0 @@
-const initialState = {
-    counter: 0
-};
-
-export default initialState;
